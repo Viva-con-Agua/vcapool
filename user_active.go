88a5bdd5f0c1fbebec7c14714179b546252022c5
dennis <dennis_kleber@mailbox.org>
2022-03-07 17:38:24 +0100

    withdraw active state

--- user_active.go
+++ user_active.go
@@ -28,19 +28,20 @@ func NewUserActive(userID string) *UserActive {
 	}
 }
 
-func (i *UserActive) Active() {
-	i.Status = "active"
+func (i *UserActive) Confirmed() {
+	i.Status = "confirmed"
 	i.Since = time.Now().Unix()
 	i.Modified.Update()
 }
 
 func (i *UserActive) Rejected() {
 	now := time.Now().Unix()
-	if i.Status == "active" {
+	if i.Status == "confirmed" {
 		i.History.Add(i.Since, now)
 	}
 	i.Status = "rejected"
 	i.Expired = now
+	i.Since = 0
 	i.Modified.Update()
 }
 
@@ -51,8 +52,37 @@ func (i *UserActive) Requested() {
 	i.Modified.Update()
 }
 
+func (i *UserActive) Withdraw() {
+	now := time.Now().Unix()
+	if i.Status == "confirmed" {
+		i.History.Add(i.Since, now)
+	}
+	i.Status = "withdrawn"
+	i.Since = 0
+	i.Expired = now
+	i.Modified.Update()
+}
+
 func (i *UserActive) IsRequested() bool {
 	if i.Status == "requested" {
+		return true
+	}
+	return false
+}
+func (i *UserActive) IsConfirmed() bool {
+	if i.Status == "confirmed" {
+		return true
+	}
+	return false
+}
+func (i *UserActive) IsWithdrawn() bool {
+	if i.Status == "withdrawn" {
+		return true
+	}
+	return false
+}
+func (i *UserActive) IsRejected() bool {
+	if i.Status == "rejected" {
 		return true
 	}
 	return false
